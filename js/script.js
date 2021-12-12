fb7221e53c249b9cf8d585789743775e06927c6f
BrentonPedler <brenton_pedler@hotmail.com>
2021-12-12 18:12:37 -0500

    reverting back to changes

--- js/script.js
+++ js/script.js
@@ -37,28 +37,19 @@ $(".portfolioLink").click(function() {
 
 // CLICK EVENT FOR HAMBURGER MENU
 
-// const menuDropdown = document.querySelector('.fa-bars');
+const menuDropdown = document.querySelector('.fa-bars');
 
-// let menuOpen = false;
+let menuOpen = false;
 
-// menuDropdown.addEventListener('click', () => {
-//   if (!menuOpen) {
-//     menuDropdown.classList.add('open'); 
-//     menuOpen = true;
-//   } else {
-//     menuDropdown.classList.remove('open');
-//     menuOpen = false;
-//   }
-// })
-
-const toggleMenu = () => {
-  const menu = document.querySelector('.fa-bars');
-
-  if (menu.style.display == "block") {
-    menu.style.display = "none";
+menuDropdown.addEventListener('click', () => {
+  if (!menuOpen) {
+    menuDropdown.classList.add('open'); 
+    menuOpen = true;
   } else {
-    menu.style.display = "block";
+    menuDropdown.classList.remove('open');
+    menuOpen = false;
   }
-}
+})
+
 
 
