c9156979dc6d5eb248d70cdad8ec09a6ffafe9c5
BrentonPedler <brenton_pedler@hotmail.com>
2022-02-13 14:55:29 -0500

    added carousel

--- js/script.js
+++ js/script.js
@@ -50,5 +50,27 @@ document.querySelectorAll('.item').forEach(e => e.addEventListener('click', () =
   console.log("click");
 }))
 
+// PROJECT CAROUSEL
+
+const buttons = document.querySelectorAll('[data-carousel-button]');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        // next or previous image
+        const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
+        const slides = button
+            .closest('[data-carousel]')
+            .querySelector('[data-slides]');
+        
+            const activeSlide = slides.querySelector('[data-active]');
+            let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+            if (newIndex < 0) newIndex = slides.children.length - 1;
+            if (newIndex >= slides.children.length) newIndex = 0;
+
+            slides.children[newIndex].dataset.active = true;
+            delete activeSlide.dataset.active;
+    })
+})
+
 
 
